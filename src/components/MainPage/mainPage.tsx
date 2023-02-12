7268bf7c3c132e7e66b61be9894b9a0c050bb7db
AleksandraBoch <shemaFoxy@gmail.com>
2023-02-12 22:50:07 +0300

    first page ended

--- src/components/MainPage/mainPage.tsx
+++ src/components/MainPage/mainPage.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import './mainPage.module.css'
+// import '.../media.saas'
+import {Button} from "@mui/material";
+import { InputBase } from '@mui/material';
+import TextField from "@mui/material/TextField";
+
 
 export const MAinScreen=()=>{
     return(
@@ -6,19 +12,34 @@ export const MAinScreen=()=>{
 
            <div>
                <div>Логин</div>
-               <input/>
+               <TextField
+                   // style={{justifyContent: "space-between"}}
+                   size={'small'}
+                   variant={'outlined'}
+                   label={'логин'}
+               />
            </div>
             <div>
                 <div>Пароль</div>
-                <input/>
+                <TextField
+                    size={'small'}
+                    variant={'outlined'}
+                    // onChange={onChangeSetLocalItem}
+                    // onKeyDown={onEnterDownAddItem}
+                    label={'пароль'}
+                    // error={error}
+                    // helperText={error&&'Title is required'}
+                />
             </div>
             <span>
-    <button >LogIn</button>
+    <Button variant={"contained"}>LogIn</Button>
 </span>
             <div>
                <img src={'https://peterburg.center/sites/default/files/12_otel_spb_0.jpg'}/>
             </div>
 <input type={'datetime'}/>
+
+
         </div>
     )
 }
